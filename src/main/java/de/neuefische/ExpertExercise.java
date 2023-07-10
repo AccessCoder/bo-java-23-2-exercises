a27d35c610114aa17f587ef1dd1697b474c1d34c
Dominic <andro5@web.de>
2023-07-10 17:23:47 +0200

    update checkSum for forLoop and WhileLoop

--- src/main/java/de/neuefische/ExpertExercise.java
+++ src/main/java/de/neuefische/ExpertExercise.java
@@ -30,6 +30,27 @@ public class ExpertExercise {
         return lastDigit + checkSum(number / 10);
     }
 
+    public static int checkSumWhile(int number) {
+        int solution = 0;
+
+        while (number != 0){
+            solution += number%10;
+            number /= 10;
+        }
+
+        return solution;
+    }
+
+
+    public static int checkSumFor(int number) {
+        int solution = 0;
+
+        for (int i;number!=0;number=number/10){
+            solution+=number%10;
+        }
+        return solution;
+    }
+
     public static String decimalToRoman(int num) {
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] romanLiterals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
